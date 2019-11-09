4f60df50878bbb59a70025fd0f11b98148da92fb
Franklin Tarter <franklintarter@gmail.com>
2019-11-09 14:03:19 -0800

    finish up initial version

--- example-site/src/pages/index.js
+++ example-site/src/pages/index.js
@@ -8,30 +8,30 @@ import { SEO, useSEO } from "gatsby-plugin-seo"
 const IndexPage = () => { 
   const { siteUrl } = useSEO()
   return (
-  <Layout>
-    <SEO
-      title="Home"
-      description="Description of the site/home page."
-      pagePath="/"
-      schema={`{
-            "@context": "http://schema.org",
-            "@type": "WebPage",
-            "mainEntity": {
-              "@type": "Organization",
-              "name": "Example Company",
-              "image": "${siteUrl}/img/logo.png"
-            }
-          }`}
-    />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-        }
+    <Layout>
+      <SEO
+        title="Home"
+        description="Description of the site/home page."
+        pagePath="/"
+        schema={`{
+              "@context": "http://schema.org",
+              "@type": "WebPage",
+              "mainEntity": {
+                "@type": "Organization",
+                "name": "Example Company",
+                "image": "${siteUrl}/img/logo.png"
+              }
+            }`}
+      />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
