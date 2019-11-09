c23c8c7d7ed9ad71e6e29fb152cda5c2d069d53b
Franklin Tarter <franklintarter@gmail.com>
2019-11-09 13:10:29 -0800

    rename example

---
+++ example-site/gatsby-config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-seo",
+      options: {
+        siteName: "Example Company",
+        defaultSiteImage: "/img/logo.png",
+        siteUrl: "https://example.com",
+        twitterCreator: "@twitterhandle",
+        twitterSite: "@twitterhandle",
+        globalSchema: `{
+            "@type": "WebSite",
+            "@id": "https://example.com/#website",
+            "url": "https://example.com/",
+            "name": "Example Site Title",
+            "publisher": {
+              "@id": "https://example.com/about/#organization"
+            },
+            "image": {
+              "@type": "ImageObject",
+              "@id": "https://example.com/#logo",
+              "url": "https://example.com/img/logo.png",
+              "caption": "Example Company Logo"
+            }
+          }`,
+      },
+    },
+  ],
+}
---
+++ example-site/src/pages/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import { SEO } from "gatsby-plugin-seo"
+
+const IndexPage = () => { 
+  const siteUrl = "https://example.com" // TODO from query
+  return (
+  <Layout>
+    <SEO
+      title="Home"
+      description="Description of the site/home page."
+      pagePath="/"
+      schema={`{
+            "@context": "http://schema.org",
+            "@type": "WebPage",
+            "mainEntity": {
+              "@type": "Organization",
+              "name": "Example Company",
+              "image": "${siteUrl}/img/logo.png"
+            }
+          }`}
+    />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+        }
+
+export default IndexPage
